43b447cc7bd89328992f011a3ecbbfcb6762ed45
prabeen <74868630+prabeenprabu@users.noreply.github.com>
2022-07-27 02:40:05 -0400

    change some things and add some functions.

--- script/script.js
+++ script/script.js
@@ -1,6 +1,19 @@
+//creating variables
 var calDisplay = document.getElementById("display");
 var music = document.getElementById("play");
+var musicDisplay = document.getElementById("musicName");
+var musicList = new Array();
+var musicDir = new Array();
+var leftMove = document.getElementById("left");
+var rightMove = document.getElementById("right");
+var index = 0;
+//adding values
+musicList.push("Beep","Error","Metal","TypeWriter");
+musicDir.push("music/beep.wav","music/error.wav","music/metal.wav","music/type.wav");
 
+
+musicDisplay.innerHTML = musicList[index];
+//functions
 function display(val){
     music.play();
     if(calDisplay.value.startsWith('E')){
@@ -16,7 +29,7 @@ function display(val){
     else{
         calDisplay.value += val;
     }    
-}
+};
 function answer(){
     music.play();
     if(calDisplay.value != ""){
@@ -32,14 +45,39 @@ function answer(){
         calDisplay.value = "Enter Value !"
     }
     
-}
+};
 function back(){
     music.play();
     var num = calDisplay.value;
     calDisplay.value = num.slice(0,-1);
-}
+};
 function clean(){
     music.play();
     calDisplay.value = "";
     calDisplay.style.color = "#141414";
-}
+};
+
+var setMusic = (name,index) => {
+    music.src = name;
+    musicDisplay.innerText = musicList[index];
+};
+rightMove.onclick = function() {
+    if(index <= 2){        
+        index++;
+        setMusic(musicDir[index],index);
+    }
+    else{
+        index = 0;
+        setMusic(musicDir[index],index);
+    }
+};
+leftMove.onclick = function(){
+    if(index == 0){        
+        index = 3;
+        setMusic(musicDir[index],index);
+    }
+    else{
+        index--;
+        setMusic(musicDir[index],index);
+    }
+};
